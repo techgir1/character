103b3e6b1d5f579b1074efa73a752a4fcd95f429
Beth Fostergit config --global user.namegit config --global user.name Beth <beth@techgir1.com>
2016-01-13 22:21:41 -0800

    added js sample

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(function () {
+  $("").click(function(){
+    $("").show();
+    $("").hide();
+  });
+});
+
+$(function(event){
+  $("#").click(function(){
+    var rachel = $("").val();
+    var ross = $("").val();
+    var phoebe = $("").val();
+    var chander = $("").val();
+    var joey = $("").val();
+
+    if ( === "" &&  === "") {
+      $('').show();
+    } else if ( === "" &&  === "") {
+      $('').show();
+    } else if ( === "" &&  === "") {
+      $('').show();
+    } else {
+      $('').show();
+    }
+    
+    $("").hide();
+    $("").show();
+  });
+});
+
+
+
+
